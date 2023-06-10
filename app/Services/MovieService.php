84abe67508b181a5b90d7857517b07decdecc13a
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-10 13:30:58 -0600

    VideoStore | Simplify logic, add todo, fix error

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -6,39 +6,41 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Cache\CacheManager;
 use Illuminate\Support\Facades\Cache;
 
+use function PHPUnit\Framework\throwException;
+
 class MovieService
 {
-    public const KEY_MOVIE_NUMBER  = 'movie_number_';
-    public const KEY_MOVIE_COUNTER = 'movie_counter';
+    public const KEY_MOVIE_NUMBER  = 'movieNumber_';
+    public const KEY_MOVIE_COUNTER = 'movieCounter';
 
     // Public Methods
     public function createMovies($movieTitle): JsonResponse
     {
-
-        $this->createMovie($movieTitle);
-       // return $this->getTotalKeysInCache();
-
-        return  response()->json(['success' => 'success', 200]);
+        //todo validate movieTitle.
+        return $this->createMovie($movieTitle);
     }
 
-
-    // Public Methods
-
-    private function createMovie($movieTitle): void
+    private function incrementMovieCounter(): void
     {
-        $movieKey = self::KEY_MOVIE_NUMBER . $this->getMovieCounter() + 1;
-        if (! Cache::has($movieKey)) {
-            $movieValue = [
-                'title' => $movieTitle,
-                'likes' => $movieTitle
-            ];
-            Cache::add($movieKey, $movieValue);
-        }
+        Cache::increment(self::KEY_MOVIE_COUNTER);
     }
-    private function getMovieCounter()
+
+    private function getMovieCounter(): int
     {
         return Cache::get(self::KEY_MOVIE_COUNTER, 0);
     }
 
+    // Private Methods
 
+    private function createMovie($movieTitle): JsonResponse
+    {
+        $this->incrementMovieCounter();
+        $newMovieKey = self::KEY_MOVIE_NUMBER . $this->getMovieCounter();
+        $newMovieValue = [
+                'title' => $movieTitle,
+                'likes' => 0
+        ];
+        Cache::add($newMovieKey, $newMovieValue);
+        return response()->json(['success' => 'success', 200]);
+    }
 }
