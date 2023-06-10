e5c1531668f3f9b6f34073c71682d0d7e3ecc395
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-09 23:28:14 -0600

    VideoStore | Add validations to Movie POST Request

--- app/Http/Requests/MoviePostRequest.php
+++ app/Http/Requests/MoviePostRequest.php
@@ -3,13 +3,32 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class MoviePostRequest extends FormRequest
 {
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'Movie title is required',
+            ];
+    }
     public function rules(): array
     {
         return [
             'title' => 'required|max:255',
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+            ]
+        ));
     }
 }
