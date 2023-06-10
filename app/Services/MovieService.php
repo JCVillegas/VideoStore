e3fa5f1aaaa9b67631b85428e7e0461121c57251
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-10 15:32:54 -0600

    VideoStore | Create helper method to get all movies

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -3,10 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Cache\CacheManager;
 use Illuminate\Support\Facades\Cache;
-
-use function PHPUnit\Framework\throwException;
 
 class MovieService
 {
@@ -20,27 +17,43 @@ class MovieService
         return $this->createMovie($movieTitle);
     }
 
-    private function incrementMovieCounter(): void
+    // Private Methods
+
+    private function createMovie($movieTitle): JsonResponse
     {
-        Cache::increment(self::KEY_MOVIE_COUNTER);
+        $this->incrementMovieCounter();
+        $newMovieKey   = self::KEY_MOVIE_NUMBER . $this->getMovieCounter();
+        $newMovieValue = [
+            'title' => $movieTitle,
+            'likes' => 0
+        ];
+        Cache::add($newMovieKey, $newMovieValue);
+        return response()->json(['success' => 'success', 200]);
+    }
+
+    private function getAllMovies(): array
+    {
+        $movieCounter = $this->getMovieCounter();
+        $movieArray   = range(0, $movieCounter - 1);
+        foreach ($movieArray as $movieNumber) {
+            $movieKey                  = self::KEY_MOVIE_NUMBER . $movieNumber + 1;
+            $movie                     =  $this->getMovie($movieKey);
+            $movieArray [$movieNumber] = $movie;
+        }
+        return $movieArray;
+    }
+
+    private function getMovie($movieKey): array
+    {
+        return Cache::get($movieKey, 0);
     }
 
     private function getMovieCounter(): int
     {
         return Cache::get(self::KEY_MOVIE_COUNTER, 0);
     }
-
-    // Private Methods
-
-    private function createMovie($movieTitle): JsonResponse
+    private function incrementMovieCounter(): void
     {
-        $this->incrementMovieCounter();
-        $newMovieKey = self::KEY_MOVIE_NUMBER . $this->getMovieCounter();
-        $newMovieValue = [
-                'title' => $movieTitle,
-                'likes' => 0
-        ];
-        Cache::add($newMovieKey, $newMovieValue);
-        return response()->json(['success' => 'success', 200]);
+        Cache::increment(self::KEY_MOVIE_COUNTER);
     }
 }
