4f7fb0d1445254550213b069c4ed679afa23f31e
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-09 20:48:27 -0600

    VideoStore | Create Api Controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('movie', function(Request $request) {
+    return Article::create($request->all);
+});
