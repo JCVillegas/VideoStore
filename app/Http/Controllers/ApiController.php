dbee840a3164c092d31967356d8c66702942d56d
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-11 09:30:32 -0600

    VideoStore | Add Controller method for adding a like to a movie

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MoviePostLikeRequest;
 use App\Http\Requests\MoviePostRequest;
 use App\Services\MovieService;
 use Illuminate\Http\JsonResponse;
 
 class ApiController extends Controller
 {
+    public function addMovieLike($movieId): JsonResponse
+    {
+        (new MovieService())->addMovieLikes($movieId);
+
+        return response()->json(['success' => 'success', 200]);
+    }
+
     public function createMovie(MoviePostRequest $request): JsonResponse
     {
         return (new MovieService())->createMovies($request->title);
