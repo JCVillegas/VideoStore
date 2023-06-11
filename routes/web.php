548697727f807ec6593880298adb8e21553e8434
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-11 15:40:24 -0600

    VideoStore | Simplify web routes and add missing route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/videoCatalog', function () {
-    return view('newMovie');
-});
+Route::view('/', 'welcome');
+Route::view('/videoCatalog', 'viewMovieCatalog');
+Route::view('/newMovie', 'newMovie');
