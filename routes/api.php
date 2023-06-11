259a86fd9a6993c5644bf2f4f6448a3b9ed04557
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-10 18:22:32 -0600

    VideoStore | Add routes for getMovies and deleteMovies

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('movie', [ApiController::class, 'createMovie']);
+Route::get('movies', [ApiController::class, 'getMovies']);
+
+Route::delete('movies', [ApiController::class, 'deleteMovies']);
