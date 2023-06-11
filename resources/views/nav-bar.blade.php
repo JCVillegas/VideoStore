307e6cb44b8f044ac133769d8779e5a19424ac4f
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-11 15:38:31 -0600

    VideoStore | Create navigation menu

---
+++ resources/views/nav-bar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/videoCatalog') }}">Video Catalog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ URL::to('/newMovie') }}">New Movie</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
