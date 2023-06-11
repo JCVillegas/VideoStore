1568e0a8aae6f318a2c38ed83a34ca45e42e7210
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-11 10:47:38 -0600

    VideoStore | Add request in body

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(ApiController::class)->group(function () {
-    Route::post('movie/{movieId}/likes', 'addMovieLike');
+    Route::post('movie/likes', 'addMovieLike');
     Route::post('movie', 'createMovie');
     Route::get('movies', 'getMovies');
     Route::delete('movies', 'deleteMovies');
