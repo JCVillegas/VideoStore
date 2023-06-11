d217c8c28effc94f71766296ed192b0b6a91dbd0
Juan Villegas <juancarlos.villegas@gmail.com>
2023-06-10 18:22:14 -0600

    VideoStore | Add deleteMovie helper method, and validation for getAllMovies, in case there are not results

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -16,15 +16,29 @@ class MovieService
         return $this->createMovie($movieTitle);
     }
 
+    public function deleteMovies(): void
+    {
+        for ($i = 1; $i <= $this->getMovieCounter(); $i++) {
+            $movieKey = self::KEY_MOVIE_NUMBER . $i;
+            $this->deleteMovie($movieKey);
+        }
+        $this->deleteMovieCounter();
+    }
+
     public function getAllMovies(): array
     {
         $movieCounter = $this->getMovieCounter();
+        if ($movieCounter === 0) {
+            return [];
+        }
         $movieArray   = range(0, $movieCounter - 1);
         foreach ($movieArray as $movieNumber) {
             $movieKey                  = self::KEY_MOVIE_NUMBER . $movieNumber + 1;
             $movie                     =  $this->getMovie($movieKey);
             $movieArray [$movieNumber] = $movie;
         }
+        $movieArray ['movieCounter'] = $movieCounter;
+
         return $movieArray;
     }
 
@@ -43,6 +57,15 @@ class MovieService
         return response()->json(['success' => 'success', 200]);
     }
 
+    private function deleteMovieCounter(): void
+    {
+        Cache::delete(self::KEY_MOVIE_COUNTER);
+    }
+    private function deleteMovie($movieKey): void
+    {
+        Cache::delete($movieKey);
+    }
+
     private function getMovie($movieKey): array
     {
         return Cache::get($movieKey, 0);
@@ -52,6 +75,7 @@ class MovieService
     {
         return Cache::get(self::KEY_MOVIE_COUNTER, 0);
     }
+
     private function incrementMovieCounter(): void
     {
         Cache::increment(self::KEY_MOVIE_COUNTER);
